ff865508c46828a7154acbcd01fc2d9db1fd104f
Luka <lukacindric3@gmail.com>
2020-09-08 14:41:43 -0700

    inital commit with basic project setup and sign in form

---
+++ RPAQuiz/common/RPAQuiz.cs
@@ -0,0 +1,23 @@
+﻿using RPAQuiz.features.sign_in.views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPAQuiz
+{
+    static class RPAQuiz
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SignInScreen());
+        }
+    }
+}
---
+++ RPAQuiz/features/sign_in/views/SignInScreen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPAQuiz.features.sign_in.views
+{
+    public partial class SignInScreen : Form
+    {
+        public SignInScreen()
+        {
+            InitializeComponent();
+            SetupUI();
+        }
+
+        //UI setup
+        private void SetupUI()
+        {
+            SetupTitleBar();
+        }
+
+        private void SetupTitleBar()
+        {
+            this.Text = "Prijava";
+        }
+    }
+}
